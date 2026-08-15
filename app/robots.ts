b24cf36a3723abdd0edcbf965663677c4a88b047
roquerodriguez27-dotcom <roquerodriguez27@gmail.com>
2026-08-15 12:07:50 -0400

    Tighten robots configuration

--- app/robots.ts
+++ app/robots.ts
@@ -1,8 +1,16 @@
 import type { MetadataRoute } from "next";
+import { SITE } from "@/lib/site-config";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: "https://www.floridasoutheastrealty.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/"],
+      },
+    ],
+    sitemap: `${SITE.url}/sitemap.xml`,
+    host: SITE.url,
   };
 }

5371539b5c58ddd857a66e67d0f44f20b66812e4
roquerodriguez27-dotcom <roquerodriguez27@gmail.com>
2026-08-15 12:00:20 -0400

    Add verified client reviews

---
+++ lib/testimonials.ts
@@ -0,0 +1,32 @@
+export interface ClientReview {
+  name: string;
+  quote: string;
+}
+
+export const CLIENT_REVIEWS: ClientReview[] = [
+  {
+    name: "JC",
+    quote:
+      "Can’t say enough about the A++ service I received from Roque. Very knowledgeable and definitely knows how to work deals. If getting great service, a good deal and the home of your dreams is for you, then this is who you call. Highly recommend — can’t go wrong!",
+  },
+  {
+    name: "Giuseppe Oliveri",
+    quote:
+      "Moved from New York to Palm Beach and Roque was my realtor who went above and beyond to find me my dream home. Thanks, Roque.",
+  },
+  {
+    name: "Yamile Larrosa",
+    quote:
+      "This process can truly be stressful, but when you have people who know what they are doing and care about their customers, the process is much easier. My experience with Roque was amazing. He was never too busy to take a call or show a house. Thank you for helping us find our dream home!",
+  },
+  {
+    name: "Kat",
+    quote:
+      "Roque is the best in the business. He has helped my husband and me find multiple businesses. He goes above and beyond to make sure he gets the deal closed and to make sure you get the most for your money. I highly recommend Roque!",
+  },
+  {
+    name: "Marco Larrosa",
+    quote:
+      "I worked with Roque about four years ago on two opportunities, and I can’t say enough about how he helped us find the right place and guided us through the process to the end. He did an excellent job and was very professional. I highly recommend him.",
+  },
+];
